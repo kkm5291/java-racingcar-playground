4649c893d98b914db52062aace2ff5fb67a65668
kyungmo kim <kkm5291@posod.co.kr>
2024-12-27 17:53:16 +0900

    [feat] 입력기능 추가

--- src/main/java/racing/InputUtils.java
+++ src/main/java/racing/InputUtils.java
@@ -1,13 +1,22 @@
 package racing;
 
+import java.util.Scanner;
+
 public class InputUtils {
 
     public static final int NAME_LIMIT_LENGTH = 5;
+    public static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 5글자 이하만 가능합니다";
+
+    public static final Scanner sc = new Scanner(System.in);
 
     public static boolean nameValidation(String name) {
         if (name.length() > NAME_LIMIT_LENGTH) {
-            throw new RuntimeException("이름은 5글자 이하만 가능합니다");
+            throw new RuntimeException(NAME_LENGTH_EXCEPTION_MESSAGE);
         }
         return true;
+    }
+
+    public static String inputValue() {
+        return sc.nextLine();
     }
 }
